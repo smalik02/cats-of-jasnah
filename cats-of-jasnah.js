c63556cf7333a713c85a665851b6f6472d002703
Shehryar Malik <shehryar.malik@tufts.edu>
2019-12-29 22:37:14 -0500

    Language update
    SpeechSynthesisUtterance instead of google translate cuz idk

--- cats-of-jasnah.js
+++ cats-of-jasnah.js
@@ -82,10 +82,21 @@ const speak = function(text, opts) {
   opts = opts || {}
   $('p').text(text)
 //   responsiveVoice.speak(text, 'US English Female', opts)
-  var audio = new Audio()
-  audio.src =`http://translate.google.com/translate_tts?ie=utf-8&tl=ur&q=${text}`
-  console.log(audio.src);
-  audio.play();
+  var msg = new SpeechSynthesisUtterance();
+  var voices = window.speechSynthesis.getVoices();
+  msg.voice = voices[10]; // Note: some voices don't support altering params
+  msg.voiceURI = 'native';
+  msg.volume = 1; // 0 to 1
+  msg.rate = 1; // 0.1 to 10
+  msg.pitch = 2; //0 to 2
+  msg.text = text;
+  msg.lang = 'ur-PK';
+
+  msg.onend = function(e) {
+    console.log('Finished in ' + event.elapsedTime + ' seconds.');
+  };
+
+  speechSynthesis.speak(msg);
 }
 
 var make_cats = function() {
